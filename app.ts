a44547a14dffbfed68515e60514a2c10638f762d
Roberta Ferreira Aguilar <roberta.aguilar@kobel.com.br>
2023-05-15 08:59:07 -0300

    Item 1

--- app.ts
+++ app.ts
@@ -1,9 +1,11 @@
-import { CompanyAccount } from './class/CompanyAccount'
-import { PeopleAccount } from './class/PeopleAccount'
+import { PeopleAccount } from './class/PeopleAccount';
 
-const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
-console.log(peopleAccount)
-peopleAccount.deposit()
-const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
-console.log(companyAccount)
+const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Robs', 10);
+// console.log(peopleAccount)
+// peopleAccount.deposit()
+// const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
+// companyAccount.deposit()
+// console.log(companyAccount)
+
+peopleAccount.deposit(50);
+peopleAccount.withdraw(40);
--- class/DioAccount.ts
+++ class/DioAccount.ts
@@ -1,42 +1,48 @@
 export abstract class DioAccount {
-  private name: string
-  private readonly accountNumber: number
-  balance: number = 0
-  private status: boolean = true
+  private name: string;
+  private readonly accountNumber: number;
+  balance: number = 0;
+  private status: boolean = true;
 
-  constructor(name: string, accountNumber: number){
-    this.name = name
-    this.accountNumber = accountNumber
+  constructor(name: string, accountNumber: number) {
+    this.name = name;
+    this.accountNumber = accountNumber;
   }
 
   setName = (name: string): void => {
-    this.name = name
-    console.log('Nome alterado com sucesso!')
-  }
+    this.name = name;
+    console.log('Nome alterado com sucesso!');
+  };
 
   getName = (): string => {
-    return this.name
-  }
+    return this.name;
+  };
 
-  deposit = (): void => {
-    if(this.validateStatus()){
-      console.log('Voce depositou')
+  deposit = (balance: number): void => {
+    this.balance = balance;
+    if (this.validateStatus()) {
+      console.log(`Voce depositou ${balance} reais!`);
     }
-  }
+  };
 
-  withdraw = (): void => {
-    console.log('Voce sacou')
-  }
+  withdraw = (amount: number): void => {
+    if (this.validateStatus() && amount <= this.balance) {
+      console.log(`Voce sacou ${amount} reais!`);
+    } else {
+      console.log(this.validateStatus() ? 'Saldo insuficiente!' : 'Conta inativa');
+    }
+
+  };
 
   getBalance = (): void => {
-    console.log(this.balance)
-  }
+    console.log(this.balance);
+  };
 
   private validateStatus = (): boolean => {
     if (this.status) {
-      return this.status
+      return this.status;
     }
 
-    throw new Error('Conta inválida')
-  }
+    throw new Error('Conta inválida');
+  };
 }
